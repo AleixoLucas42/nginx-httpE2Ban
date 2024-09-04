b27742de3103b4b5224614d3ad66780c2cccfbaf
Lucas Aleixo da Silva <lucas.silva@2rpnet.com>
2024-09-04 00:51:31 -0300

    Update: print info when block ip

--- main.py
+++ main.py
@@ -211,6 +211,7 @@ def block_ip(ip):
                 new_content.append("}")
             with open(os.getenv("BANNED_CONF_FILE", "banned.conf"), "w") as banned_file:
                 banned_file.writelines(new_content)
+                logging.info(f"IP {ip} successfuly banned")
             reload_nginx()
         except Exception as e:
             logging.error(f"Some error occur while trying to block IP: '{ip}', {e}")
